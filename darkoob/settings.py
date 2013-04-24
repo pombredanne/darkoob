036e319c1e22d6367b74324a673ab1de616892e5
vahid kharazi <vahid@kharazi.net>
2013-04-25 01:48:24 +0430

    set static's file url in urls.py and static' s path in seting.py

--- darkoob/settings.py
+++ darkoob/settings.py
@@ -1,5 +1,9 @@
 # Django settings for darkoob project.
 
+# Get root of project 
+import os.path
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -61,7 +65,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+# STATIC_ROOT = '/home/vahid/Workspace/darkoob/static/'
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -69,6 +74,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    ROOT + '/static/',
+    #'/home/vahid/Workspace/darkoob/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
