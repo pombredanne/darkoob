47e825478a7bbbdea4ac205910df544956b028c3
vahid kharazi <vahid@kharazi.net>
2013-04-25 12:19:26 +0430

    Clickjacking Protection

--- darkoob/views.py
+++ darkoob/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.utils.translation import ugettext as _
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 
 def HomePage(request):
     return HttpResponse("HELLO DARKOOB! :D")
@@ -8,3 +10,9 @@ def HomePage(request):
 
 def BasicPage(request):
 	return render_to_response('index.html', {})
+
+
+@xframe_options_exempt
+def ok_to_load_in_a_frame(request):
+    return HttpResponse("This page is safe to load in a frame on any site.")
+
