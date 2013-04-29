941b898c7e2fa899113d38537401bea4f8932229
vahid kharazi <vahid@kharazi.net>
2013-04-29 23:53:04 +0430

    Add another dajaxice example

--- darkoob/ajax.py
+++ darkoob/ajax.py
@@ -4,3 +4,8 @@ from dajaxice.decorators import dajaxice_register
 @dajaxice_register
 def dajaxice_example(request):
     return simplejson.dumps({'message':'Hello from Python!'})
+
+
+@dajaxice_register
+def args_example(request, text):
+    return simplejson.dumps({'message':'Your message is %s!' % text})
