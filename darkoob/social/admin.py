70d861012458c54048170ffe6ead749481ec6fe1
vahid kharazi <vahid@kharazi.net>
2013-04-30 20:58:44 +0430

    Add List View To UserProfile Admin

--- darkoob/social/admin.py
+++ darkoob/social/admin.py
@@ -4,5 +4,7 @@ from models import UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('user', 'sex')
 	search_fields = ('user',)
+	list_display = ('user', 'sex', 'birthday')
+
 	
 admin.site.register(UserProfile,UserProfileAdmin)
