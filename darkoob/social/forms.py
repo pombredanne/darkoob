777775413e49cf2dd6318737db9f189914fff3a6
vahid kharazi <vahid@kharazi.net>
2013-04-30 19:13:30 +0430

    Registration Form

--- darkoob/social/forms.py
+++ darkoob/social/forms.py
@@ -1,2 +1,95 @@
 from django import forms
 
+SEX_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+)
+MONTH_CHOICES = (
+	(1,'1'),
+	(2,'2'),
+	(3,'3'),
+	(4,'4'),
+	(5,'5'),
+	(6,'6'),
+	(7,'7'),
+	(8,'8'),
+	(9,'9'),
+	(10,'10'),
+	(11,'11'),
+	(12,'12'),
+)
+class RegisterForm(forms.Form):
+	first_name = forms.CharField(label="firstname", 
+		min_length=2, 
+		max_length=30,
+		widget=forms.TextInput(attrs={
+			'label':'First Name',
+		}))
+
+	last_name = forms.CharField(label="lastname",min_length=2,max_length=30,widget=forms.TextInput(attrs={
+		'place':'Last Name',
+		'label':'Last Name',
+		'require':'true',
+		}))
+	email = forms.EmailField(label="email",min_length=5,widget=forms.TextInput(attrs={
+		'place': 'Your email address',
+		'label':'Email', 
+		'require':'true',
+		'id':'email',
+		'validate':'email',	
+		}))
+	pw = forms.CharField(label="Password", min_length=8,max_length=30,widget=forms.PasswordInput(attrs={
+		'place':'Password',
+		'label':'Password',
+		'require':'true',
+		'id':'pass',
+		}))
+	pwconfirm = forms.CharField(label="rePassword",min_length=8,max_length=30, widget=forms.PasswordInput(attrs={
+		'place':'Re-type password',
+		'label':'rePassword',
+		'require':'true',
+		'match':'pw',
+		}))
+	sex = forms.ChoiceField(choices=SEX_CHOICES)
+	
+	month = forms.ChoiceField(choices=MONTH_CHOICES)
+	day=forms.CharField(label="Day",min_length=1,max_length=2,widget=forms.TextInput(attrs={
+		'place':"Day",
+		'require':'true',
+		'label':'Day',
+		'size':'1',
+		}))
+		
+	year=forms.CharField(label="Year",min_length=4,max_length=4,widget=forms.TextInput(attrs={
+		'place':"Year",
+		'require':'true',
+		'label':'Year',
+		'size':'1',
+		}))
+		
+	
+	def clean_day(self):	
+ 		day = int(self.cleaned_data['day'])
+		print type(day),"itssssssssssssssssss type"
+		if (day>31 or day<1):
+			raise forms.ValidationError("Invalid date")
+			
+
+		return day
+	def clean_month(self):
+		month= int(self.cleaned_data['month'])
+		if (month >12 or month<1):
+			raise forms.ValidationError("Invalid date")
+		
+		return month
+	def clean_year(self):
+		year= int(self.cleaned_data['year'])
+		if (year >2012 or year<1900):
+			raise forms.ValidationError("Invalid date")
+		
+		return year
+		
+
+
+
+
--- darkoob/social/views.py
+++ darkoob/social/views.py
@@ -1,22 +1,55 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from darkoob.social.forms import RegisterForm
 
 def signup(request):
-    if request.method != 'POST':
-        return render(request, 'index.html', {
-            'errors': [_('an internal error has happend.')]
-        })
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        username = request.POST['username']
-        password = request.POST['password1']
-        user = User.objects.create_user(username=username, password=password)
-        return HttpResponseRedirect(reverse('social:login'))
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data      
+        email=cd["email"]
+#TODO:check email to isnt in db,email should be unique 
+        if cd["pw"]==cd["pwconfirm"]:
+            user=User.objects.create_user(username=cd["email"],password=cd["pw"],email=cd["email"])
+            user.first_name =cd["first_name"]
+            user.last_name=cd["last_name"]
+        print dir(user)
+
+        print UserProfile.objects.filter(user=user).update(birthday=str(cd["year"]) + " "+ str(cd["month"]) + " " + str(cd["day"]))
+        if cd["sex"]=="Female":
+            UserProfile.objects.filter(user=user).update(src='female.jpg')
+            UserProfile.objects.filter(user=user).update(sex=False)
+        else:
+            UserProfile.objects.filter(user=user).update(src='male.jpg')
+            
+
+        user.save()
+
+        return render_to_response('registered.html',{'firstname':cd['firstname']})
+    else:
+        form = RegisterForm(
+            initial={'username': 'I love your site!'}
+        )
+    return render_to_response('signup.html', {'form': form})
+
+
+
+    # if request.method != 'POST':
+    #     return render(request, 'signup.html', {
+    #         'errors': [_('an internal error has happend.')]
+    #     })
+    # form = UserCreationForm(request.POST)
+    # if form.is_valid():
+    #     username = request.POST['username']
+    #     password = request.POST['password1']
+    #     user = User.objects.create_user(username=username, password=password)
+    #     return HttpResponseRedirect(reverse('social:login'))
 
 
 @login_required
