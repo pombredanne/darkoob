dc229325a7a527c2d2143b105b47899140018e32
vahid kharazi <vahid@kharazi.net>
2013-04-30 20:20:57 +0430

    Add UserProfile Model To Django Admin Website

--- darkoob/social/admin.py
+++ darkoob/social/admin.py
@@ -2,12 +2,7 @@ from django.contrib import admin
 from models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-	list_display = ('user',)
-	# list_display = ('user','sex')
-	# search_fields = ('user',)
-    # list_filter = ('sex',ist_display = ('user','sex')
-	# search_fields = ('user',)
-    # list_filter = ('sex','birthplace')'birthplace')
-
-
+	list_display = ('user', 'sex')
+	search_fields = ('user',)
+	
 admin.site.register(UserProfile,UserProfileAdmin)
