13015279b548547af399be62e4a9760bfae85b38
vahid kharazi <vahid@kharazi.net>
2013-05-08 00:58:10 +0430

    Get Ajax Result Url

--- darkoob/search/views.py
+++ darkoob/search/views.py
@@ -5,5 +5,20 @@ from django.contrib.auth.models import User
 
 
 
-def index_user( request ):
+def user_search_page( request ):
     return render_to_response('index_user.html',{},context_instance = RequestContext(request))
+
+def ajax_result_of_user_search( request ):
+    if request.is_ajax():
+        q = request.GET.get('q')
+        if q is not None:            
+            results = User.objects.filter( 
+                Q( first_name__contains = q ) |
+                Q( last_name__contains = q ) |
+                Q( username__contains = q ) )
+            return render_to_response('results_user.html',{'results':results,}, 
+                                       context_instance = RequestContext(request))
+
+
+
+    return render_to_response('results_user.html',{})
--- darkoob/urls.py
+++ darkoob/urls.py
@@ -5,6 +5,7 @@ from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from darkoob import views 
 from darkoob import ajax 
 
+from search.views import *
 
 dajaxice_autodiscover()
 admin.autodiscover()
@@ -23,4 +24,10 @@ urlpatterns = patterns('',
     # Project-specific URLs
     url(r'^$', views.index, name='index'),
     url(r'', include('darkoob.social.urls', namespace='social', app_name='socials')),
+
+
+    # Search 
+    # TODO: Move to search application
+    url(r'^user/$',user_search_page),
+    url(r'^users/$',ajax_result_of_user_search),
 )
