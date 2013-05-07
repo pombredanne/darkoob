a1b38879b06d7c6ce51cdc1c3a767dfbf873329b
vahid kharazi <vahid@kharazi.net>
2013-05-07 23:51:53 +0430

    Add Dajax Example

--- darkoob/ajax.py
+++ darkoob/ajax.py
@@ -9,3 +9,12 @@ def dajaxice_example(request):
 @dajaxice_register
 def args_example(request, text):
     return simplejson.dumps({'message':'Your message is %s!' % text})
+
+import random
+from dajax.core import Dajax
+
+@dajaxice_register
+def randomize(request):
+    dajax = Dajax()
+    dajax.assign('#result', 'value', random.randint(1, 10))
+    return dajax.json()
