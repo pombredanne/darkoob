e2f83a6a5d43ebc52d03d4059a7526a579a425c1
vahid kharazi <vahid@kharazi.net>
2013-05-08 15:03:14 +0430

    Set User Profile Unicode Function

--- darkoob/social/models.py
+++ darkoob/social/models.py
@@ -9,6 +9,8 @@ class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	sex = models.CharField(max_length = 6, choices = SEX_CHOICES)
 	birthday = models.DateField(null = True)
+	def __unicode__(self):
+		return self.user.get_full_name()
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
