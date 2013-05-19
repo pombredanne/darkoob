18e13c15f5a03b27fd3dbe9e973f6d2600563d7b
vahid kharazi <vahid@kharazi.net>
2013-05-19 11:10:10 +0430

    Add __unicode__ Function to Book Models | Add Books Models to Django Admin

--- darkoob/book/models.py
+++ darkoob/book/models.py
@@ -1,32 +1,52 @@
 from django.db import models
 from darkoob.social.models import User
 
-# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
     publisher = models.ForeignKey('Publisher')
     language = models.ForeignKey('Language')
     authors = models.ManyToManyField('Author')
 
+    def __unicode__(self):
+        return unicode(self.title)
+
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return unicode(self.name)
 
 class Language(models.Model):
     name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return unicode(self.name)
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return unicode(self.name)
+
 class Translator(models.Model):
     author = models.OneToOneField(Author)
+
+    def __unicode__(self):
+        return self.author
 
 class Translation(models.Model):
     book = models.ForeignKey(Book)
     translator = models.ForeignKey(Translator)
     language = models.ForeignKey(Language)
 
+    def __unicode__(self):
+        return self.book
+
 class Review(models.Model):
     book = models.ForeignKey(Book)
     user = models.ForeignKey(User)
     text = models.TextField()
     submitted_time = models.DateField()
+
+    def __unicode__(self):
+        return unicode(self.book) + unicode(self.user)
