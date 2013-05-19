ab1e073e0284f0eb0a0f83a445cff25b88bf0863
vahid kharazi <vahid@kharazi.net>
2013-05-19 20:01:56 +0430

    Change in Group Admin

--- darkoob/group/admin.py
+++ darkoob/group/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from models import Group, Schedule, Deadline
 
-
 class GroupAdmin(admin.ModelAdmin):
     search_fields = ('name', 'admin')
-    list_display = ('name', 'admin', 'members')
+    list_display = ('name', 'admin')
 
 class ScheduleAdmin(admin.ModelAdmin):
     search_fields = ('group', 'book')
@@ -12,7 +11,7 @@ class ScheduleAdmin(admin.ModelAdmin):
 
 class DeadlineAdmin(admin.ModelAdmin):
     search_fields = ('schedule', 'end_time')
-    list_display = ('group', 'from_page', 'to_page', 'end_time')
+    list_display = ('schedule', 'from_page', 'to_page', 'end_time')
 
 
 admin.site.register(Group, GroupAdmin)
