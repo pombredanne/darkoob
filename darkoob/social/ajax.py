3df79b37981c4c34aa92b84661f9b5a890bf2fc6
vahid kharazi <vahid@kharazi.net>
2013-05-21 02:38:55 +0430

    Ajax to social and few changes

--- darkoob/social/ajax.py
+++ darkoob/social/ajax.py
@@ -1,10 +1,19 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 
+from models import UserProfile
+
 @dajaxice_register(method='POST')
-def edit_sex(request,sex):
+def edit_sex(request, sex):
 	errors = []
-	done = False
-	print 'day:', sex
-	return simplejson.dumps({'done': done, 'sex': sex, 'errors': errors})
+
+    # try:
+    #     UserProfile.objects.filter(user=request.user).update(sex=sex)
+    # except:
+    #     errors.append('An error ecoured while change information')
+    # else:
+    #     done = True
+	# print 'day:',sex
+    done = True
+	return simplejson.dumps({'done': done,'sex': 'Male', 'errors': errors})
 
