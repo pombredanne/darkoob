5e09043213834ca447822c5078c05d2cd6c29c4e
Aryan Baghi <ar.baghi.ce@gmail.com>
2013-05-26 19:55:42 +0430

    Post application has been added

---
+++ darkoob/post/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from darkoob.social.models import User
+from darkoob.group.models import Group
+
+class Post(models.Model):
+	user_id = models.ForeignKey(User)
+	text = models.TextField()
+	submitted_time = models.DateTimeField()
+
+	def __unicode__(self):
+        return unicode(self.text)
+
+class GroupPostStream(models.Model):
+	group_id = models.ForeignKey(Group)
+	post_id = models.ForeignKey(Post)
+
+	def __unicode__(self):
+		return unicode("group_id: %d    post_id: %d"%(self.group_id, self.post_id))
+
+class CommentStream(models.Model):
+	post_id = models.ForeignKey(Post)
+	parent_id = models.ForeignKey(Post)
+
+	def __unicode__(self):
+		return unicode("Comment with post_id: %d comes for Post with id: %s"%(self.post_id, parent_id))
+
+class ProfilePostStream(models.Model):
+	user_id = models.ForeignKey(User)
+	post_id = models.ForeignKey(Post)
+
+	def __unicode__(self):
+		return unicode("Post with id: %d belong to user with id: %d"%(self.post_id, self.user_id))
