b51ae87c16a168e4b405c7d1435c9616b1184884
vahid kharazi <vahid@kharazi.net>
2013-05-27 09:11:29 +0430

    add releated name and admin for post

---
+++ darkoob/post/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from models import Post, GroupPostStream, CommentStream, ProfilePostStream
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'submitted_time')
+    search_fields = ('user', 'submitted_time', 'text')
+
+class GroupPostStreamAdmin(admin.ModelAdmin):
+    list_display = ('group_id', 'post_id')
+    search_fields = ('group_id', 'post_id')
+
+class CommentStreamAdmin(admin.ModelAdmin):
+    list_display = ('post_id', 'parent_id')
+    search_fields = ('post_id',)
+
+class ProfilePostStreamAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'post_id')
+    search_fields = ('user_id', 'post_id')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(GroupPostStream, GroupPostStreamAdmin)
+admin.site.register(CommentStream, CommentStreamStream)
+admin.site.register(ProfilePostStream, ProfilePostStreamAdmin)
--- darkoob/settings.py
+++ darkoob/settings.py
@@ -160,6 +160,7 @@ INSTALLED_APPS = (
     'darkoob.social',
     'darkoob.search',
     'darkoob.group',
+    'darkoob.post',
     
 )
 
