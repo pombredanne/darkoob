07015f1a2e3b5fcda58e774b93b7b6f05af850d0
Afshin Rodgar <afshin.chichi@gmail.com>
2013-05-28 14:42:14 +0430

    A little bit of progress

--- darkoob/book/search_indexes.py
+++ darkoob/book/search_indexes.py
@@ -1,11 +1,18 @@
 from haystack import indexes
+#from queued_search.indexes import QueuedSearchIndex
 from darkoob.book.models import Book, Author
 
 
 class BookIndexes(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    publisher = indexes.CharField(model_attr='publisher')
-    language = indexes.CharField(model_attr='language')
+    #publisher = indexes.CharField(model_attr='publisher')
+    #language = indexes.CharField(model_attr='language')
 
     def get_model(self):
         return Book
+
+class AuthorIndexes(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Author
