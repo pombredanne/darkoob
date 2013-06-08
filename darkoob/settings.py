f827706b54bc6fea31c3884a94913dffa4cfe1f0
vahid kharazi <vahid@kharazi.net>
2013-06-09 00:24:28 +0430

    change settings

--- darkoob/settings.py
+++ darkoob/settings.py
@@ -81,13 +81,11 @@ LOGIN_URL = 'social:login'
 SITE_ROOT = os.path.dirname(__file__)
 THUMBNAIL_DEBUG = True
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
-print "khkhkkhkhk",MEDIA_ROOT
-MEDIA_URL = '/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
