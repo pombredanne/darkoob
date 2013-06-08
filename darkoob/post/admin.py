01b716fcd6132fb82374c05a5f23049253cfe496
Sina Mahmoodi <itz.s1na@gmail.com>
2013-06-08 17:36:20 +0430

    Change post template a little, fix attributes of post model

--- darkoob/post/admin.py
+++ darkoob/post/admin.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from darkoob.post.models import Post, GroupPostStream, CommentStream, ProfilePostStream
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'submitted_time')
-    search_fields = ('user_id', 'submitted_time', 'text')
+    list_display = ('user', 'submitted_time')
+    search_fields = ('user', 'submitted_time', 'text')
 
 class GroupPostStreamAdmin(admin.ModelAdmin):
-    list_display = ('group_id', 'post_id')
-    search_fields = ('group_id', 'post_id')
+    list_display = ('group', 'post')
+    search_fields = ('group', 'post')
 
 class CommentStreamAdmin(admin.ModelAdmin):
-    list_display = ('post_id', 'parent_id')
-    search_fields = ('post_id',)
+    list_display = ('post', 'parent')
+    search_fields = ('post',)
 
 class ProfilePostStreamAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'post_id')
-    search_fields = ('user_id', 'post_id')
+    list_display = ('user', 'post')
+    search_fields = ('user', 'post')
 
 
 admin.site.register(Post, PostAdmin)
