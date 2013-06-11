99e1f51ccc9ed40d180bf79d94b1b4d72bc393dd
vahid kharazi <vahid@kharazi.net>
2013-06-12 00:55:19 +0430

    clean up forms

--- darkoob/group/forms.py
+++ darkoob/group/forms.py
@@ -3,7 +3,14 @@ from django.utils.translation import ugettext as _
 from django.contrib.auth.forms import AuthenticationForm
 
 class GroupForm(forms.Form):
-    name = forms.CharField(max_length=255)
+    name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={
+            'placeholder': _('Group Name'),
+            'class': 'input-medium ',
+            'required': '',
+        })
+    )
     members = forms.CharField(widget=forms.HiddenInput())
 
 
