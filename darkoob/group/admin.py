fdfc5e797fd0f4b3e45ca6ea750028d7ceec88ff
vahid kharazi <vahid@kharazi.net>
2013-07-04 13:07:09 +0430

    Add post to group admin

--- darkoob/group/admin.py
+++ darkoob/group/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Group, Schedule, Deadline
+from models import Group, Schedule, Deadline, Post
 
 class GroupAdmin(admin.ModelAdmin):
     search_fields = ('name', 'admin')
@@ -13,7 +13,12 @@ class DeadlineAdmin(admin.ModelAdmin):
     search_fields = ('schedule', 'end_time')
     list_display = ('schedule', 'from_page', 'to_page', 'end_time')
 
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('user',)
+    list_display = ('group', 'user', 'submitted_time')
 
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Deadline, DeadlineAdmin)
+admin.site.register(Post, PostAdmin)
+
