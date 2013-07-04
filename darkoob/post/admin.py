7a6b4d8267cc5512d253ed69e19a48fa82b0cab8
vahid kharazi <vahid@kharazi.net>
2013-07-04 11:51:04 +0430

    Remove *Steame from django admin

--- darkoob/post/admin.py
+++ darkoob/post/admin.py
@@ -1,24 +1,11 @@
 from django.contrib import admin
-from darkoob.post.models import Post, GroupPostStream, CommentStream, ProfilePostStream
+from darkoob.post.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('user', 'submitted_time')
     search_fields = ('user', 'submitted_time', 'text')
 
-class GroupPostStreamAdmin(admin.ModelAdmin):
-    list_display = ('group', 'post')
-    search_fields = ('group', 'post')
-
-class CommentStreamAdmin(admin.ModelAdmin):
-    list_display = ('post', 'parent')
-    search_fields = ('post',)
-
-class ProfilePostStreamAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post')
-    search_fields = ('user', 'post')
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(GroupPostStream, GroupPostStreamAdmin)
-admin.site.register(CommentStream, CommentStreamAdmin)
-admin.site.register(ProfilePostStream, ProfilePostStreamAdmin)
+
