97bc166f77a29ea5e49bc78d24def238748670b2
Sina Mahmoodi <itz.s1na@gmail.com>
2013-07-08 03:07:45 +0430

    Change syncdb

--- darkoob/syncdb.sh
+++ darkoob/syncdb.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-cd ..
 grep backends.sqlite darkoob/settings.py
 if [ $? -eq 0 ]; then
+  cd ..
   rm dardb
+  python darkoob/test/drop_nodes.py
+  python manage.py syncdb
   cp dardb darkoob/
+  cd darkoob
+  python test/test.py
+  mv dardb ../
 else
   grep backends.postgres darkoob/settings.py
   if [ $? -eq 0 ]; then
     dropdb dardb
     createdb dardb
-  fi
-fi
-python darkoob/test/drop_nodes.py
-if [ $? -eq 0 ]; then
-  python manage.py syncdb
-  if [ $? -eq 0 ]; then
-    cd darkoob
+    python ../manage.py syncdb
     python test/test.py
-    if [ -f dardb ]; then
-      mv dardb ../
-    fi
   fi
 fi
